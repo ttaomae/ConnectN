2774a15a12e0a9eb658def9bd548dcb7580fa2d0
Todd Taomae <ttaomae@gmail.com>
2014-05-21 10:59:48 -1000

    add optional parameters to client Main

--- src/main/java/ttaomae/connectn/network/client/Main.java
+++ src/main/java/ttaomae/connectn/network/client/Main.java
@@ -1,6 +1,7 @@
 package ttaomae.connectn.network.client;
 
 import java.io.IOException;
+import java.util.List;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -9,14 +10,27 @@ import javafx.stage.Stage;
 
 public class Main extends Application
 {
+    private static final int DEFAULT_PORT = 4200;
     @Override
     public void start(Stage stage) throws Exception
     {
+        // get host and port from parameters
+        List<String> args = getParameters().getRaw();
+        String hostname = "localhost";
+        int portNumber = DEFAULT_PORT;
+        if (args.size() == 2) {
+            hostname = args.get(0);
+            try {
+                portNumber = Integer.parseInt(args.get(0));
+            } catch (NumberFormatException e) {
+                System.err.println("Using default port number.");
+            }
+        }
+
         stage.setTitle("Client");
 
         BorderPane root = new BorderPane();
-        root.setCenter(new ClientPanel("localhost", 4200));
-
+        root.setCenter(new ClientPanel(hostname, portNumber));
         stage.setScene(new Scene(root, 450, 400));
         stage.show();
     }
