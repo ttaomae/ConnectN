f6f49a0a365f0dc91740214a53b55e34168017ab
Todd Taomae <ttaomae@gmail.com>
2014-05-23 17:30:43 -1000

    refactor Client

--- src/main/java/ttaomae/connectn/network/client/Client.java
+++ src/main/java/ttaomae/connectn/network/client/Client.java
@@ -17,6 +17,8 @@ import ttaomae.connectn.network.ConnectNProtocol;
 public class Client implements Runnable
 {
     private Socket socket;
+    private PrintWriter socketOut;
+    private BufferedReader socketIn;
 
     private Player player;
     /** This client's copy of the board */
@@ -26,6 +28,9 @@ public class Client implements Runnable
             throws UnknownHostException, IOException
     {
         this.socket = new Socket(hostname, portNumber);
+        this.socketOut = new PrintWriter(socket.getOutputStream(), true);
+        this.socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
         this.player = player;
         this.board = board;
     }
@@ -33,70 +38,74 @@ public class Client implements Runnable
     @Override
     public void run()
     {
-        try (
-            PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader socketIn =
-                new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        ) {
-            boolean rematch = true;
-            while (rematch) {
-                System.out.println("CLIENT: Starting game");
+        boolean rematch = true;
+        while (rematch) {
+            System.out.println("CLIENT: Starting game");
 
-                // make sure the board is empty by undoing everything
-                // TODO: kind of hack-y. must ensure that the panel and client have the same board
-                while (this.board.undoPlay());
+            // make sure the board is empty by undoing everything
+            // TODO: kind of hack-y. must ensure that the panel and client have the same board
+            while (this.board.undoPlay());
 
-                // play a full game
-                while (this.board.getWinner() == Piece.NONE) {
-                    try {
-                        // get message from server
-                        String message = socketIn.readLine();
-
-                        // server is waiting for move
-                        if (message.equals(ConnectNProtocol.READY)) {
-                            // get a move and play it
-                            int move = -1;
-                            while (!this.board.isValidMove(move)) {
-                                move = player.getMove(this.board.copy());
-                            }
-                            this.board.play(move);
-
-                            socketOut.println(ConnectNProtocol
-                                    .constructMove(move));
-                        }
-                        // server sent opponent move
-                        else if (ConnectNProtocol.verifyMove(message)) {
-                            // play opponent's move
-                            this.board.play(ConnectNProtocol.parseMove(message));
-                        }
-                    } catch (IOException e) {
-                        System.err.println("Error reading from socket.");
-                    }
-                }
-
-                System.out.println(this.board.getWinner() + " wins!");
+            try {
+                playGame();
 
                 String message = socketIn.readLine();
                 if (message.equals(ConnectNProtocol.REMATCH)) {
-                    // TODO: very hack-y
-                    System.out.println("Type 'y' for a rematch.");
-                    if (new Scanner(System.in).nextLine().equals("y")) {
-                        socketOut.println(ConnectNProtocol.YES);
-                        rematch = socketIn.readLine().equals(ConnectNProtocol.YES);
-                    }
-                    else {
-                        socketOut.println(ConnectNProtocol.NO);
-                        rematch = false;
-                    }
+                    rematch = getRematch();
                 }
-                // server did not send expected message
+                // did not get a rematch message from the server, so quit
                 else {
                     rematch = false;
                 }
+            } catch (IOException e) {
+                System.err.println("Error reading from socket.");
+                rematch = false;
             }
-        } catch (IOException e) {
-            System.err.println("Couldn't connect to server");
         }
         System.out.println("Done!");
+    }
+
+    private void playGame() throws IOException
+    {
+        while (this.board.getWinner() == Piece.NONE) {
+            // get message from server
+            String message = this.socketIn.readLine();
+
+            // server is waiting for move
+            if (message.equals(ConnectNProtocol.READY)) {
+                // get a move and play it
+                int move = -1;
+                while (!this.board.isValidMove(move)) {
+                    move = player.getMove(this.board.copy());
+                }
+                this.board.play(move);
+
+                this.socketOut.println(ConnectNProtocol.constructMove(move));
+            }
+            // server sent opponent move
+            else if (ConnectNProtocol.verifyMove(message)) {
+                // play opponent's move
+                this.board.play(ConnectNProtocol.parseMove(message));
+            }
+        }
+
+        System.out.println(this.board.getWinner() + " wins!");
+    }
+
+    private boolean getRematch() throws IOException
+    {
+        // get input from user
+        // TODO: very hack-y
+        System.out.println("Type 'y' for a rematch.");
+        if (new Scanner(System.in).nextLine().equals("y")) {
+            socketOut.println(ConnectNProtocol.YES);
+            // wait for server response; rematch if opponent agrees
+            return socketIn.readLine().equals(ConnectNProtocol.YES);
+
+        }
+        else {
+            socketOut.println(ConnectNProtocol.NO);
+            return false;
+        }
     }
 }
