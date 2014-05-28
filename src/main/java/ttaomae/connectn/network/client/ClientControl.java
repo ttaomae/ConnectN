72f0f95f41372ffbe2f79d296f136c48ef7004c8
Todd Taomae <ttaomae@gmail.com>
2014-05-28 11:38:43 -1000

    replace ClientPanel
    Created ClientControl to replace ClientPanel. Uses FXML to define the
    layout. Allows the user to specify the server they wish to connect to.

---
+++ src/main/java/ttaomae/connectn/network/client/ClientControl.java
@@ -0,0 +1,131 @@
+package ttaomae.connectn.network.client;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import ttaomae.connectn.Board;
+import ttaomae.connectn.gui.BoardPanel;
+import ttaomae.connectn.gui.MousePlayer;
+import ttaomae.connectn.network.ConnectNProtocol;
+
+public class ClientControl extends BorderPane implements ClientListener
+{
+    @FXML private TextField hostField;
+    @FXML private TextField portField;
+    @FXML private Button connectButton;
+
+    @FXML private BoardPanel boardPanel;
+
+    @FXML private Label displayMessage;
+
+    private boolean connected;
+
+    public ClientControl()
+    {
+        initialize();
+        load();
+
+        this.connected = false;
+
+        // display a default board to start
+        this.boardPanel.setBoard(new Board());
+    }
+
+    private void initialize()
+    {
+        this.hostField = new TextField();
+        this.portField = new TextField();
+        this.connectButton = new Button();
+        this.boardPanel = new BoardPanel();
+        this.displayMessage = new Label();
+    }
+
+    private void load()
+    {
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(getClass().getResource("/layout/client.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    @FXML
+    private void connect()
+    {
+        if (!this.connected) {
+            try {
+                String host = hostField.getText();
+                int port = Integer.parseInt(portField.getText());
+                Board b = new Board();
+                this.boardPanel.setBoard(b);
+                MousePlayer mp = new MousePlayer(this.boardPanel);
+                Client c = new Client(host, port, mp, b);
+                c.addListener(this);
+
+                Thread myThread = new Thread(c);
+                myThread.setDaemon(true);
+                myThread.start();
+
+                this.updateMessage(String.format("Connected to %s:%d%n", host, port));
+                this.connected = true;
+                this.connectButton.setDisable(true);
+            }
+            catch (NumberFormatException e) {
+                this.updateMessage("Invalid port number.");
+            }
+            catch (IOException e) {
+                this.updateMessage("Could not connect to server.");
+            }
+        }
+    }
+
+    /**
+     * Sets the display to the specified message.
+     *
+     * @param message the message to update to
+     */
+    public void updateMessage(final String message)
+    {
+        javafx.application.Platform.runLater(new Runnable() {
+            @Override
+            public void run()
+            {
+                ClientControl.this.displayMessage.setText(message);
+            }
+        });
+    }
+
+    @Override
+    public void clientReceivedMessage(String message)
+    {
+        switch (message) {
+            case ConnectNProtocol.START:
+                this.updateMessage("Starting game...");
+                break;
+            case ConnectNProtocol.READY:
+                this.updateMessage("Select your move");
+                break;
+            case ConnectNProtocol.REMATCH:
+                this.updateMessage("Game Over! Rematch?");
+                break;
+        }
+    }
+
+    @Override
+    public void clientSentMessage(String message)
+    {
+        if (message.startsWith(ConnectNProtocol.MOVE)) {
+            this.updateMessage("Waiting for opponent...");
+        }
+    }
+}
--- src/main/java/ttaomae/connectn/network/client/Main.java
+++ src/main/java/ttaomae/connectn/network/client/Main.java
@@ -1,7 +1,6 @@
 package ttaomae.connectn.network.client;
 
 import java.io.IOException;
-import java.util.List;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -10,28 +9,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application
 {
-    private static final int DEFAULT_PORT = 4200;
     @Override
     public void start(Stage stage) throws Exception
     {
-        // get host and port from parameters
-        List<String> args = getParameters().getRaw();
-        String hostname = "localhost";
-        int portNumber = DEFAULT_PORT;
-        if (args.size() == 2) {
-            hostname = args.get(0);
-            try {
-                portNumber = Integer.parseInt(args.get(1));
-            } catch (NumberFormatException e) {
-                System.err.println("Using default port number.");
-            }
-        }
-
         stage.setTitle("Client");
 
         BorderPane root = new BorderPane();
-        root.setCenter(new ClientPanel(hostname, portNumber));
-        stage.setScene(new Scene(root, 450, 400));
+        root.setCenter(new ClientControl());
+        stage.setScene(new Scene(root, 450, 500));
+        stage.setResizable(false);
         stage.show();
     }
 
